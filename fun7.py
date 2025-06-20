edc97370d632ee3c10a9b07261c47f9a9ff76091
Banupriya0812 <banuven5@gmail.com>
2025-06-20 14:36:56 +0530

    Add files via upload

---
+++ fun7.py
@@ -0,0 +1,43 @@
+
+#          1. Check if a number is even or odd.
+# def is_even_or_odd(n):
+#     if n%2==0:
+#         return "even"
+#     else:
+#         return "odd"
+# print(is_even_or_odd(3))
+# print(is_even_or_odd(10))
+
+
+
+        # Reverse a string.
+# def reverse_string(s):
+#     return s[::-1]
+# print(reverse_string("maadhava"))
+
+
+
+        # Calculate the factorial of a number.
+# def factorial(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+# print(factorial(7))
+
+
+        # Find the largest element in a list.
+# def find_largest_element(list):
+#     if not list:
+#         return None
+#     return max(list)
+# print(find_largest_element([10,5,20,15]))
+
+
+          # Count the occurrences of each character in a string.
+# from collections import Counter
+
+# def count_characters(s):
+#     return Counter(s)
+
+# print(count_characters("maadhava"))
