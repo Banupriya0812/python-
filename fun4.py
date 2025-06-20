9686e906d4a76f266722ef316d0b5b9822a772f7
Banupriya0812 <banuven5@gmail.com>
2025-06-20 11:28:37 +0530

    Add files via upload

---
+++ fun4.py
@@ -0,0 +1,84 @@
+
+  #dictionary key:value
+
+# thisdict =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# x = thisdict["model"]
+# print(x)
+
+
+    #change dict values 
+# thisdict = {
+#     "brand" : "ford",
+#     "model" : "i10",
+#     "year" : 2020
+# }
+# thisdict["year"] =2023
+
+# print(thisdict)
+
+
+     #update values  
+# thisdict ={
+#     "brand " : "ford",
+#     "model" :"mustang ",
+#     "year" : 2020
+# }
+# thisdict.update({"year":2025})
+# print(thisdict)
+
+
+
+       #  adding values 
+# thisdict = {
+#     "brand " :"ford",
+#     "model" : "i10",
+#     "year" : 2020
+# }
+# thisdict["color"] ="red"
+# print(thisdict)
+
+
+    #  loop dict
+# thisdict = {
+#     "brand" :"ford",
+#     "model" :"i10",
+#     "year ":"2020"
+# }
+# for x in thisdict:  #only key is displayed 
+#     print(x)        # print(thisdict[x]) values are displayed , 
+
+
+
+
+    #   other method 
+# thisdict = {
+#     "brand" : "ford",
+#     "model" : "i10",
+#     "year" :2020
+# }
+# for x in thisdict.values():
+#   print(x)
+  
+
+# thisdict = {
+#     "brand" : "ford",
+#     "model" : "i10",
+#     "year" :2020
+# }
+# for x in thisdict.keys():
+#   print(x)
+
+
+#      copy dict
+# thisdict = {
+#     "brand" : "ford",
+#     "model" : "i10",
+#     "year" :2020
+# }
+# mydict=thisdict.copy()
+# print(mydict)
+
