0aa8f202940eb3c547608ca9810e9ebb3ee55061
Banupriya0812 <banuven5@gmail.com>
2025-06-24 12:07:06 +0530

    Add files via upload

---
+++ fun10.py
@@ -0,0 +1,51 @@
+     # array is a variable holds more than one value at a time 
+
+# car=["hundai", "toyota", "i10"]
+# print(car)
+
+
+        # array is given with index value (a[0]).........
+
+# car=["hundai", "toyota", "i10"]
+# print(car[0])
+
+
+      # length of array 
+# car=["hundai", "toyota", "i10"]
+# print(len(car))
+
+
+        # looping of array 
+
+# car=["hundai", "toyota", "i10"]
+
+# for s in car:
+#  print(s)
+
+
+         # adding index to array
+# car=["hundai", "toyota", "i10"]
+
+# car.append("honda")
+# print(car)
+
+
+
+     # delete index to array
+# car=["hundai", "toyota", "i10"]
+
+# car.pop(2)
+# print(car)
+
+
+     # remove  index from  array
+# car=["hundai", "toyota", "i10","honda","volvo"]
+
+# car.remove("honda")
+# print(car)
+
+     # clear  index from array
+# car=["hundai", "toyota", "i10"]
+
+# car.clear()
+# print(car)
